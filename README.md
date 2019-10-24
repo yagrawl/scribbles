776a147d3c5334739b36f0e01af99996267c2910
Yash Agrawal <yagrawl2@gmail.com>
2019-10-24 08:19:11 -0700

    basic theme setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yash Agrawal's Blog
+
+Powered by Gatsby
---
+++ src/components/bio.js
@@ -0,0 +1,52 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
+
+function Bio() {
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { author } = data.site.siteMetadata
+        return (
+          <Container>
+            <p>
+              Written by <strong>{author}</strong> in San
+              Francisco.
+            </p>
+          </Container>
+        )
+      }}
+    />
+  )
+}
+
+const bioQuery = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author
+      }
+    }
+  }
+`
+
+const Container = styled.div`
+  display: flex;
+`
+
+export default Bio
