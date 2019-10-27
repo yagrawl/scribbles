665f8d7884e683906b49b92dfd7bd2524742b094
Yash Agrawal <yagrawl2@gmail.com>
2019-10-27 06:20:28 +0530

    fix margins

--- src/components/layout.js
+++ src/components/layout.js
@@ -61,7 +61,7 @@ class Layout extends React.Component {
             marginLeft: `auto`,
             marginRight: `auto`,
             maxWidth: rhythm(24),
-            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+            padding: `${rhythm(0.5)} ${rhythm(3 / 4)}`,
           }}
         >
           <header>{header}</header>
