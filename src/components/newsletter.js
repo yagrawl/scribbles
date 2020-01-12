f3c900d8f70eff305be49ad89d1b9c76178e8ded
Yash Agrawal <yagrawl2@gmail.com>
2020-01-11 17:40:55 -0800

    setup for basic newsletter operations

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import '../styles/base.scss';
 import '../styles/_animation.scss';
@@ -9,7 +10,7 @@ class Newsletter extends Component {
 
     this.state = {
       email: '',
-      loading: false,
+      status: 'INIT',
       active: false
     }
 
@@ -47,39 +48,90 @@ class Newsletter extends Component {
 
   submit() {
     if(this.state.active) {
-      let email = { "email": this.state.email, "subscribed": true }
+      this.setState(
+        prevState => ({
+          ...prevState,
+          status: 'LOADING'
+        })
+      );
 
-      console.log('Submitted');
+      let email = { "email": this.state.email, "subscribed": true };
+      axios.post(`http://localhost:4000/api/scribbles/subscribe`,
+        email)
+      .then(res => {
+        if(res.status === 200) {
+          if(res.data === "Subscribed") {
+            this.setState(
+              prevState => ({
+                ...prevState,
+                status: 'SUBSCRIBED'
+              })
+            );
+          } else if(res.data === "Email already exists") {
+              this.setState(
+                prevState => ({
+                  ...prevState,
+                  status: 'EXISTS'
+                })
+              );
+          }
+        } else {
+          this.setState(
+            prevState => ({
+              ...prevState,
+              status: 'FAILURE'
+            })
+          );
+        }
+      })
     } else {
       console.log('Please provide a valid email');
     }
   }
 
   render() {
-    return (
-      <div className="newsletter">
-        <p className="related-posts-title">Get Updates</p>
-        <p className="newsletter-description">I generally write about frontend
-        development, traveling and other interesting things. I will send an
-        email once a month. Sign up below if you are interested.</p>
-        <input className="newsletter-email-box"
-               id="newsletter"
-               type="text"
-               name="email"
-               autoComplete="off"
-               placeholder="Email"
-               value={this.state.email}
-               onChange={this.handleChange} />
-        <button
-              className={this.state.active === true ?
-                "newsletter-submit-button-active" :
-                "newsletter-submit-button-inactive"}
-              onClick={() => this.submit()}
-              >
-        Subscribe
-        </button>
-      </div>
-    );
+    if(this.state.status === "INIT") {
+      return (
+        <div className="newsletter">
+          <p className="related-posts-title">Get Updates</p>
+          <p className="newsletter-description">I generally write about frontend
+          development, traveling and other interesting things. I will send an
+          email once a month. Sign up below if you are interested.</p>
+          <input className="newsletter-email-box"
+                 id="newsletter"
+                 type="text"
+                 name="email"
+                 autoComplete="off"
+                 placeholder="Email"
+                 value={this.state.email}
+                 onChange={this.handleChange} />
+          <button
+                className={this.state.active === true ?
+                  "newsletter-submit-button-active" :
+                  "newsletter-submit-button-inactive"}
+                onClick={() => this.submit()}
+                >
+          Subscribe
+          </button>
+        </div>
+      );
+    } else if(this.state.status === "LOADING") {
+      return (
+        <div><p>LOADING</p></div>
+      );
+    } else if(this.state.status === "SUBSCRIBED") {
+      return (
+        <div><p>SUBSCRIBED</p></div>
+      );
+    } else if(this.state.status === "EXISTS") {
+      return (
+        <div><p>ALREADY EXISTS</p></div>
+      );
+    } else {
+      return (
+        <div><p>OOPS</p></div>
+      );
+    }
   }
 }
 
