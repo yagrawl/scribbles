8c2be36ede8f44af2017dfbe185d55a1f31b74ca
Yash Agrawal <yagrawl2@gmail.com>
2020-01-11 23:49:15 -0800

    add styling to newsletter response states

---
+++ src/components/loader.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import '../styles/_animation.scss';
+
+function Loader() {
+  return (
+    <div className="loader"></div>
+  )
+}
+
+export default Loader;
--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import Loader from './loader';
+
 import '../styles/base.scss';
 import '../styles/_animation.scss';
 
@@ -89,14 +91,10 @@ class Newsletter extends Component {
     }
   }
 
-  render() {
+  manageState() {
     if(this.state.status === "INIT") {
       return (
-        <div className="newsletter">
-          <p className="related-posts-title">Get Updates</p>
-          <p className="newsletter-description">I generally write about frontend
-          development, traveling and other interesting things. I will send an
-          email once a month. Sign up below if you are interested.</p>
+        <div>
           <input className="newsletter-email-box"
                  id="newsletter"
                  type="text"
@@ -117,21 +115,70 @@ class Newsletter extends Component {
       );
     } else if(this.state.status === "LOADING") {
       return (
-        <div><p>LOADING</p></div>
+        <Loader/>
       );
     } else if(this.state.status === "SUBSCRIBED") {
       return (
-        <div><p>SUBSCRIBED</p></div>
+        <div className="center">
+          <center>
+            <svg className="checkmark"
+                 xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 52 52">
+                 <circle class="checkmark__circle"
+                         cx="26" cy="26" r="25" fill="none"/>
+                 <path class="checkmark__check" fill="none"
+                         d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+            </svg>
+            <p className="related-posts-title newsletter-response">
+              Subscribed.
+            </p>
+          </center>
+        </div>
       );
     } else if(this.state.status === "EXISTS") {
       return (
-        <div><p>ALREADY EXISTS</p></div>
+        <center>
+          <svg className="checkmark"
+               xmlns="http://www.w3.org/2000/svg"
+               viewBox="0 0 52 52">
+               <circle className="checkmark__circle"
+                       cx="26" cy="26" r="25" fill="none"/>
+               <path className="checkmark__check" fill="none"
+                       d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+          </svg>
+          <p className="related-posts-title newsletter-response">
+            You're Already a Subscriber.
+          </p>
+        </center>
       );
     } else {
       return (
-        <div><p>OOPS</p></div>
+        <center>
+          <svg class="cross__svg"
+               xmlns="http://www.w3.org/2000/svg"
+               viewBox="0 0 52 52">
+               <circle cx="26" cy="26" r="25" className="circle"/>
+	              <path class="cross__path cross__path--right" d="M16,16 l20,20" />
+               <path class="cross__path cross__path--right" d="M16,36 l20,-20" />
+			    </svg>
+          <p className="related-posts-title newsletter-response">
+            Oops, something went wrong.
+          </p>
+        </center>
       );
     }
+  }
+
+  render() {
+    return (
+      <div className="newsletter">
+        <p className="related-posts-title">Get Updates</p>
+        <p className="newsletter-description">I generally write about frontend
+        development, traveling and other interesting things. I will send an
+        email once a month. Sign up below if you are interested.</p>
+        {this.manageState()}
+      </div>
+    )
   }
 }
 
