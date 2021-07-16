dc77c185896e5a1230c8d76f5535ded8923c9e87
Yash Agrawal <yagrawl2@gmail.com>
2021-07-15 18:38:08 -0700

    update about section with bolt

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,13 +17,13 @@ class About extends Component {
         <SEO title={"About me"}/>
         <Navbar title={title} cursor={cursor} subtitle={subtitle} author={author} location={location} root={true}/>
         <div className="about-text">
-        <p><span role="img" aria-label="wave emoji">👋</span> Hi, I'm Yash</p>
+          <p><span role="img" aria-label="wave emoji">👋</span> Hi, I'm Yash</p>
 
-        <p>I’m a full-stack software engineer currently working at Braintree in San Francisco, CA.</p>
+          <p>I’m a full-stack software engineer currently working at <a className="about-anchor anchor" target="_blank" href="https://bolt.com">Bolt</a> in San Francisco, CA. I have previously worked at <a className="about-anchor anchor" target="_blank" href="https://braintreepayments.com">Braintree</a> and <a className="about-anchor anchor" target="_blank" href="https://mastercard.com">Mastercard</a>.</p>
 
-        <p>I’m interested in fullstack development and fintech. Apart from that I love to run, play badminton, make pasta, build my vinyl collection and watch a lot of movies.</p>
+          <p>I’m interested in fullstack development, fintech and the 'quantified-self' movement. Apart from that I love to run, play badminton, make pasta and watch a lot of movies.</p>
 
-        <p>I also write from time to time. You can read it <a className="about-anchor anchor" href="/">here</a>.</p>
+          <p>I also write from time to time. You can read it <a className="about-anchor anchor" href="/">here</a>.</p>
         </div>
         <Footer/>
       </div>
